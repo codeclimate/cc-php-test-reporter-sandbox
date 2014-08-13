8b9043bf45320946474ed83bb803fc085a2ee72b
Jonathan Powers <bigblackrobot@gmail.com>
2014-08-13 09:37:17 -0400

    Blah

---
+++ !test/test2.php
@@ -0,0 +1,23 @@
+<?php 
+class MyClass2 { 
+
+  // singleton instance 
+  private static $instance; 
+
+  // private constructor function 
+  // to prevent external instantiation 
+  private __construct() { } 
+
+  // getInstance method 
+  public static function getInstance() { 
+
+    if(!self::$instance) { 
+      self::$instance = new self(); 
+    } 
+
+    return self::$instance; 
+
+  } 
+
+} 
+?>
